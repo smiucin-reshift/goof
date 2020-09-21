da90dc2a9026292e556ec37357d02dc80c5c6620
Sherif koussa <sherif@softwaresecured.com>
2020-09-20 20:28:55 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -290,3 +290,15 @@ exports.chat = {
     res.send({ ok: true });
   }
 };
+module.exports.encode = function(s) {
+  return s.replace(/&/g, "&amp;")
+          .replace(/"/g, "&quot;")
+          .replace(/'/g, "&apos;");
+};
+
+module.exports.decode = function(s) {
+  return s.replace(/&amp;/g, "&")
+          .replace(/&quot;/g, "\"")
+          .replace(/&apos;/g, "'");
+};
+
