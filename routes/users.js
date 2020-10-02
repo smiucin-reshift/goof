2205779d687ac3a94988432f473e99154c6fdf82
Sherif koussa <sherif@softwaresecured.com>
2020-10-02 19:39:51 -0400

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -28,10 +28,33 @@ router.get('/', async (req, res, next) => {
   // Log Object's where property for debug reasons:
   console.log('The Object.where property is set to: ', {}.where)
   console.log(results)
-
+  
   return res.json(encode(results))
 
 })
+
+router.put('/', async (req, res, next) => {
+var http = require('http');
+
+http.createServer(function onRequest(req, res) {
+  var body;
+  try {
+    body = handleRequest(req);
+  }
+  catch (err) {
+    res.statusCode = 500;
+    res.setHeader("Content-Type", "text/plain");
+    res.end(err.stack); // NOT OK
+    return;
+  }
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Content-Length", body.length);
+  res.end(body);
+}).listen(3000);
+return res.json()
+})
+
 
 router.head('/', async (req, res, next) => {
   // BAD: the category might have SQL special characters in it
