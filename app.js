7feac48820eee26c627f0de60913e4c9fd78e5c5
Sherif koussa <sherif@softwaresecured.com>
2020-10-02 20:09:12 -0400

    Update app.js

--- app.js
+++ app.js
@@ -82,6 +82,7 @@ app.get('/chat', routes.chat.get);
 app.put('/chat', routes.chat.add);
 app.delete('/chat', routes.chat.delete);
 app.use('/users', routesUsers)
+app.use('changeEmail', routes.update);
 
 // Static
 app.use(st({ path: './public', url: '/public' }));
